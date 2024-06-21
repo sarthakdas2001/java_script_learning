67752e1fac93fcb3a0304edb56a103cafb09a979
Sarthak Das <97245013+sarthakdas2001@users.noreply.github.com>
2024-06-21 16:42:03 +0000

    ++

---
+++ java_code/stri.js
@@ -0,0 +1,7 @@
+const a="interstellar"
+const  b=new String("oppenhimer")
+console.log(a)
+console.log(b)
+console.log("___________________________________________________-")
+console.log(a.length)
+console.log(a.toUpperCase)
