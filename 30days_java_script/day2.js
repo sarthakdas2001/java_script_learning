967f16d22c9368e4f07421c6eb3723b13a472f07
Sarthak Das <97245013+sarthakdas2001@users.noreply.github.com>
2024-07-14 18:46:29 +0000

    kjhkj

---
+++ 30days_java_script/day2.js
@@ -0,0 +1,22 @@
+console.log(45+89)
+console.log(89-45)
+console.log(45*89)
+console.log(45/89)
+console.log(45%20)
+a=10
+a+=a
+console.log(a)
+a-=5
+console.log(a)
+console.log(25>21)
+console.log(25<21)
+console.log(25>=25)
+console.log(25<=25)
+console.log(25==25)
+console.log(25==24)
+console.log(25===25)
+console.log(25===24)
+console.log(25>21||25<21)
+console.log(25>21&&20<21)
+console.log(25!=25)
+console.log((20 > 15) ? "Yes" : "No")
