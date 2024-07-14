eec3e4b2b1548387359c699d62984ae23eb5416f
Sarthak Das <97245013+sarthakdas2001@users.noreply.github.com>
2024-07-14 15:29:02 +0530

    Add files via upload

---
+++ 30days_java_script/day1.js
@@ -0,0 +1,23 @@
+var a=15
+let b=25
+console.log(a," ",b)
+const c=false
+console.log(c)
+let s= new String("sarthak")
+let t= new Number(45)
+let v= new Boolean(1)
+let arr=[4,8,9,7,3]
+let my= {
+    "name":"santai",
+    "age":"23",
+    "sex":"male",
+}
+console.log(s," ",t," "," ",v," ",my," ",arr)
+let num=100
+console.log(num)
+num=200
+console.log(num)
+const n=500
+console.log(n)
+// n=135 //this line will wil through a error as n is constant
+console.log(n)
